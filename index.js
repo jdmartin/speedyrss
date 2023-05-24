0c75a212414898eaeb1f698c36e15df423d8e802
jdmartin <jonsaccount+githubjd@icloud.com>
2023-05-24 01:34:19 -0700

    Refactor, delay between requests.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-//Load the config file.
+// Load the config file.
 require("dotenv").config();
 
-//Libraries
+// Libraries
 const schedule = require("node-schedule");
-
 const utils = require("./utils/utils.js");
 const heart = require("./utils/heartbeat.js");
-
-//Get some essential variables from the helper files:
 const client = utils.client;
+let Parser = require("rss-parser");
+let parser = new Parser();
 
-//Deal with previous responses
+// Deal with previous responses
 const fs = require("fs");
 const previousResponsesFile = "./data/previous_responses.json";
-
 let previousResponses = {};
 
 // Load previous responses from JSON file
@@ -22,11 +20,44 @@ if (fs.existsSync(previousResponsesFile)) {
   previousResponses = JSON.parse(previousResponsesData);
 }
 
-//Initialize the feed parser
-let Parser = require("rss-parser");
-let parser = new Parser();
+// Load feedUrls from a file
+const feedUrls = JSON.parse(fs.readFileSync("./data/feedUrls.json"));
 
-//Once that's done, let's move on to main.
+// Functions
+function getRandomDelay(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min) * 1000; // Convert to milliseconds
+}
+
+async function iterateFeedUrls(feedUrls) {
+  for (const key in feedUrls) {
+    const feedUrl = feedUrls[key];
+    let feed = await parser.parseURL(feedUrl);
+    const currentResponse = feed.items.length > 0 ? feed.items[0].title : "";
+    if (previousResponses[feedUrl] !== currentResponse) {
+      console.log(
+        `Update detected for ${key} at ${new Date().toLocaleString()};`
+      );
+      const updatesChannel = client.channels.cache.get(
+        process.env.updates_channel
+      );
+
+      if (updatesChannel) {
+        let updateUrl = feedUrls[key].replace(".atom", "/latest");
+        updatesChannel.send({
+          content: `Update detected for ${key}! ${updateUrl}`,
+          username: client.user.username,
+          avatarURL: client.user.displayAvatarURL(),
+        });
+      }
+      previousResponses[feedUrl] = currentResponse;
+    }
+
+    const pauseDuration = getRandomDelay(15, 30); // Space out requests for niceness...
+    await new Promise((resolve) => setTimeout(resolve, pauseDuration));
+  }
+}
+
+//Ok, let's kick it off...
 client.once("ready", () => {
   // prints "Ready!" to the console once the bot is online
   client.user.setActivity("the wind...", { type: 2 });
@@ -38,41 +69,7 @@ client.once("ready", () => {
   heartbeat.startBeating();
 
   const job = schedule.scheduleJob("01 01 */2 * * * ", function () {
-    (async () => {
-      // Load feedUrls from a file
-      const feedUrls = JSON.parse(fs.readFileSync("./data/feedUrls.json"));
-
-      for (const key in feedUrls) {
-        const feedUrl = feedUrls[key];
-        let feed = await parser.parseURL(feedUrl);
-        const currentResponse =
-          feed.items.length > 0 ? feed.items[0].title : "";
-        if (previousResponses[feedUrl] !== currentResponse) {
-          console.log(
-            `Update detected for ${key} at ${new Date().toLocaleString()};`
-          );
-          const updatesChannel = client.channels.cache.get(
-            process.env.updates_channel
-          );
-
-          if (updatesChannel) {
-            let updateUrl = feedUrls[key].replace(".atom", "/latest");
-            updatesChannel.send({
-              content: `Update detected for ${key}! ${updateUrl}`,
-              username: client.user.username,
-              avatarURL: client.user.displayAvatarURL(),
-            });
-          }
-          previousResponses[feedUrl] = currentResponse;
-        }
-      }
-
-      // Save updated responses to JSON file
-      fs.writeFileSync(
-        previousResponsesFile,
-        JSON.stringify(previousResponses)
-      );
-    })();
+    iterateFeedUrls(feedUrls);
   });
 });
 
